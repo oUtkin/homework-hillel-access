9bd051d682c8ef005e073364c8d06dd881b43161
o_utkin <utkin.oleksii@gmail.com>
2023-05-17 23:07:03 +0300

    second commit

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ n = int(input('Enter quantity of files: '))
 for i in range(1, n + 1):
     file_list = input('Enter file name and commands (W, R, X): ').split()
     file_list = [command_dict.get(item, item) for item in file_list]
-    new_file = {file_list[0]:  [file_list[j] for j in range(1, len(file_list))]}
+    new_file = {file_list[0]: [file_list[j] for j in range(1, len(file_list))]}
     available_files.update(new_file)
 
 m = int(input('Enter quantity of operations: '))
 for k in range(1, m + 1):
-    operation_list = input('Enter operation and file name: ').split()
+    operation_list = input('Enter operation (read, write, execute) and file name: ').split()
     if operation_list[0] in available_files.get(operation_list[1][0]):
         result += 'OK\n'
     else:
